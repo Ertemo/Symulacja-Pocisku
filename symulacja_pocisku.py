b649495e4b85aef792b298ff17849d451d0e0c41
Ertemo <ertemo@wp.pl>
2022-01-16 20:56:26 +0100

    Create symulacja_pocisku.py
    Plot is generating properly with correct axes

---
+++ symulacja_pocisku.py
@@ -0,0 +1,129 @@
+"""
+Symulacja pocisku by:
+    Maciej Grabowski
+    Jakub Gryszczuk
+    AiR 2022
+"""
+#zalaczenie biblioteki graficznej tkinter i jej skladnikow
+from tkinter import Tk, Label, Button, Entry
+#zalaczenie biblioteki graficznej do animowanych wykresow
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+import math
+
+#tworzenie okna aplikacji
+root = Tk()
+#tworzenie tytulu
+myLabel = Label(root, text="Symulacja pocisku v0.2")
+#wyswietlanie okien ze zmiennymi okienkowymi
+myLabel.grid(row=0, column=1)
+#definiowanie okna do pobierania danych
+label_v0=Label(root, text="Wprowadz predkosc poczatkowa (m/s)")
+label_v0.grid(row=1,column=0)
+wejscie_v0 = Entry(root, width = 20, bg="white")
+wejscie_v0.grid(row=1,column=1)
+
+label_kat=Label(root, text="Wprowadz kat wystrzalu (deg)") 
+label_kat.grid(row=2,column=0)
+wejscie_kat = Entry(root, width = 20, bg="white")
+wejscie_kat.grid(row=2,column=1)
+#numeracja wierszy
+global wiersz
+wiersz=3
+#numeracja do tekstu (nr przeszkody)
+global j
+j=1
+#numeracja odleglosci i wysokosci
+wysokosci = []
+odleglosci = []
+#funkcja tworzaca przeszkody o numerze i
+def stworz_przeszkode (): 
+    global j
+    global wiersz
+    #tekst
+    txt1='Wysokosc przeszkody '
+    txt2='Odlegosc od przeszkody '
+    w=str(j)
+    txt1=txt1+w
+    txt2=txt2+w
+    przeszkoda_txt1=Label(root, text=txt1)
+    przeszkoda_txt1.grid(row=wiersz,column=0)
+    przeszkoda_txt2=Label(root, text=txt2)
+    przeszkoda_txt2.grid(row=wiersz,column=2)
+    #okno do wpisywania wartosci
+    przeszkoda_wysokosc=Entry(root, bg="white")
+    przeszkoda_wysokosc.grid(row=wiersz,column=1)
+    wysokosci.append(przeszkoda_wysokosc)
+    przeszkoda_odleglosc=Entry(root, bg="white")
+    przeszkoda_odleglosc.grid(row=wiersz,column=3)
+    odleglosci.append(przeszkoda_odleglosc)
+    #iteracja
+    wiersz=wiersz+1
+    j=j+1
+#tworzenie przycisku
+dodaj_przeszkode=Button(root,text="Dodaj przeszkode", command=lambda: stworz_przeszkode())
+dodaj_przeszkode.grid(row = 1,column =2)
+
+def przycisk_ok():
+    global j
+    
+    v0=wejscie_v0.get()
+    v0=float(v0)
+    kat=wejscie_kat.get()
+    kat=float(kat)
+    katr=kat*np.pi/180
+    
+    fig, ax = plt.subplots()
+    g = 9.8
+    theta = katr
+    t = 2 * v0 * np.sin(theta) / g
+    t = np.arange(0, 0.1, 0.01)
+    x = np.arange(0, 0.1, 0.01)
+    line, = ax.plot(x, v0 * np.sin(theta) * x - (0.5) * g * x**2)
+    y_max=(v0**2)*(math.sin(katr)**2)/(2*g)
+    print(y_max)
+    
+    czas=2*v0*np.sin(katr)/9.81
+    czas=float(czas)
+    zasieg=v0*np.cos(katr)*czas
+    zasieg=float(zasieg)
+    
+    iteracja=0
+    
+    zasieg=(v0*v0)/g*np.sin(2*katr)
+    def animate(i):
+        """change the divisor of i to get a faster (but less precise) animation """
+        line.set_xdata(v0 * np.cos(theta) * (t + i /100.0))
+        line.set_ydata(v0 * np.sin(theta) * (x + i /100.0) - (0.5) * g * (x + i / 100.0)**2)  
+        return line,
+    
+    
+    plt.axis([0.0, zasieg, 0.0, y_max])
+    ax.set_autoscale_on(False)
+    
+    ani = animation.FuncAnimation(fig, animate, np.arange(1, 1000))
+    plt.show()
+    writergif = animation.PillowWriter(fps=24)
+    ani.save('wykres.gif',writer=writergif)
+    
+    while iteracja<j-1:
+        print(wysokosci[iteracja].get(),' ')
+        print(odleglosci[iteracja].get())
+        iteracja=iteracja+1
+        
+    
+    '''
+    x = np.arange(0,zasieg)
+    y = x*math.tan(katr)-9.81*np.sqrt(x)/(2*math.sqrt(v0)*np.cos(math.sqrt(katr)))
+    plt.plot(x,y)
+    plt.show()
+'''
+    fig, ax = plt.subplots()
+    return None
+
+oblicz_trajektorie=Button(root, text="Oblicz", command=przycisk_ok)
+oblicz_trajektorie.grid(row=1,column=3)
+
+#jakas kluczowa rzecz
+root.mainloop() 
